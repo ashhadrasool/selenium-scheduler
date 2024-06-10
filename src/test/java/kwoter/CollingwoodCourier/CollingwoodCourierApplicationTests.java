4e042621c8f9bfa416cd6e629f8ea55382db89c5
Ashhad Rasool <ashhad.rasool22@gmail.com>
2024-06-10 16:51:38 +0500

    Collingwood Courier first commit

---
+++ src/test/java/kwoter/CollingwoodCourier/CollingwoodCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package kwoter.CollingwoodCourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollingwoodCourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
